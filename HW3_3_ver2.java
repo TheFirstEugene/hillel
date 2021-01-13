220be179e57df119c20ca75c32f134d0414adf2f
TheFirstEugene <76692928+TheFirstEugene@users.noreply.github.com>
2021-01-14 00:01:20 +0200

    Add files via upload

--- HW3_3_ver2.java
+++ HW3_3_ver2.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 //решение 3 задания с использованием тернарного оператора
 public class HW3_3_ver2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Введите желаемый год чтобы узнать столетие ");
-        Scanner sc =  new Scanner(System.in);
         int a = sc.nextInt();
-        int b = (a/100) + 1;
-        String year = (a%100==0) ? "Это " + a/100+ " столетие" : "Это " + b + " столетие";
+        int b = (a / 100) + 1;
+        String year = (a % 100 == 0) ? "Это " + a / 100 + " столетие" : "Это " + b + " столетие";
         System.out.println(year);
     }
 }
