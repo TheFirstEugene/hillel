40a4371ae05b2386fd9345250ad374a17e60909c
Евгений <loolka98@gmail.com>
2021-01-15 17:44:23 +0200

    Test for pull request

--- src/main/java/selfStudy/Array.java
+++ src/main/java/selfStudy/Array.java
@@ -11,7 +11,7 @@ public class Array {
         try{
             for (int i = 5; i > 0; i--) {
                 System.out.println(i);
-                Thread.sleep(1000);
+                Thread.sleep(500);
             }
         }catch (InterruptedException e){
             System.out.println("Главный поток исполнения прерван");
