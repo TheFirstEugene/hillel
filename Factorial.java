4849607e7fd90b05b0d5fbae3eb068d6859d3f85
TheFirstEugene <76692928+TheFirstEugene@users.noreply.github.com>
2021-01-17 23:02:06 +0200

    HW4

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package selfStudy;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число для поиска факториала");
+        System.out.println("Факториал равен " + fact(sc.nextInt()));
+
+    }
+
+    public static int fact(int n) {
+        int result;
+        if (n == 1) return 1;
+        result = fact(n - 1) * n;
+        return result;
+    }
+}
+
