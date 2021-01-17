3f080bc5fdcf77c7e5b83a5f40ebdaa9d07bf327
Евгений <loolka98@gmail.com>
2021-01-17 22:41:56 +0200

    HW4

--- src/main/java/selfStudy/Array.java
+++ src/main/java/selfStudy/Array.java
@@ -1,20 +1,49 @@
 package selfStudy;
 
-public class Array {
-    public static void main(String[] args) {
-        Thread t = Thread.currentThread();
+public class Array implements Runnable{
+    String name;
+    Thread t;
 
-        System.out.println("Текущий поток исполнения " + t);
-        t.setName("My Thread");
-        System.out.println("После изменения " + t);
-
-        try{
-            for (int i = 5; i > 0; i--) {
-                System.out.println(i);
-                Thread.sleep(500);
+    Array(String threadname){
+        name = threadname;
+        t = new Thread(this, name);
+        System.out.println("Новый поток " + t);
+        t.start();
+    }
+    public void run(){
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
             }
         }catch (InterruptedException e){
-            System.out.println("Главный поток исполнения прерван");
+            System.out.println(name  + "прерван");
         }
+        System.out.println(name + " завершен");
     }
 }
+class MultyThreadDemo{
+    public static void main(String[] args) {
+        Array ob1 = new Array("Один");
+        Array ob2 = new Array("Два");
+        Array ob3 = new Array("Три");
+        System.out.println("Один поток запущен " + ob1.t.isAlive());
+        System.out.println("Два поток запущен "+ ob2.t.isAlive());
+        System.out.println("Три поток запущен "+ ob3.t.isAlive());
+
+
+        try {
+          ob1.t.join();
+          ob2.t.join();
+          ob3.t.join();
+        }catch (InterruptedException e){
+            System.out.println("Главный поток прерван");
+        }
+        System.out.println("Один поток запущен " + ob1.t.isAlive());
+        System.out.println("Два поток запущен "+ ob2.t.isAlive());
+        System.out.println("Три поток запущен "+ ob3.t.isAlive());
+
+        System.out.println("Главный поток завершен");
+    }
+}
+
