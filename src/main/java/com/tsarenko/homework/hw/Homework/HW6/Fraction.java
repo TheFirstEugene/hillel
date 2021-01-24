41543ae9fb29c6c976dce1432c4143e8d6248313
Евгений <loolka98@gmail.com>
2021-01-24 20:08:27 +0200

    HW6

---
+++ src/main/java/com/tsarenko/homework/hw/Homework/HW6/Fraction.java
@@ -0,0 +1,18 @@
+package com.tsarenko.homework.hw.Homework.HW6;
+
+public interface Fraction {
+
+    Fraction plus(Fraction a);
+
+    Fraction minus(Fraction a);
+
+    Fraction divide(Fraction a);
+
+    Fraction multiply(Fraction a);
+
+    int getNumerator();
+
+    int getDenominator();
+
+}
+
