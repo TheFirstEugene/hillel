5cf0336f8f40ae65170a27b36fadcb082f96e88a
Евгений <loolka98@gmail.com>
2021-02-03 23:28:12 +0200

    hw9_lombok2

--- src/main/java/HW9/lombok/Genge.java
+++ src/main/java/HW9/lombok/Genge.java
@@ -1,12 +1,10 @@
 package HW9.lombok;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 public enum Genge {
     DYSTOPIAN("Dystopian"), FAIRY_TALE("Fairy tale"), ROMANCE("Romance"), CLASSIC("Classic");
     @Getter
     String genge;
-
-    Genge(String s) {
-        this.genge = s;
-    }
 }
